7c2bf3799f55a8cba22953790c826b32015090b1
Edgar <edgargc.upc@gmail.com>
2021-10-17 00:11:44 +0200

    config redis update

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,13 +1,22 @@
 import Redis from 'ioredis';
 import crypto from 'crypto';
 
-const redis = process.env.REDIS_URL ? new Redis(process.env.REDIS_URL) : undefined;
+const redis = process.env.REDIS_URL
+  ? new Redis({
+      port: parseInt(process.env.REDIS_PORT || '', 10),
+      host: process.env.REDIS_URL,
+      password: process.env.REDIS_PASSWORD,
+      tls:
+        process.env.REDIS_SSL_ENABLED && process.env.REDIS_SSL_ENABLED != 'false' ? {} : undefined
+    })
+  : undefined;
 
 export function emailToId(email: string) {
   if (process.env.REDIS_EMAIL_TO_ID_SECRET) {
     const hmac = crypto.createHmac('sha1', process.env.REDIS_EMAIL_TO_ID_SECRET);
     hmac.update(email);
-    return hmac.digest('hex');
+    const result = hmac.digest('hex');
+    return result;
   } else {
     throw new Error('REDIS_EMAIL_TO_ID_SECRET is missing');
   }
