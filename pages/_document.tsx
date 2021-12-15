2a59f77bbd88b6421d8c508d1ddc2530761c3834
Edgar <edgargc.upc@gmail.com>
2021-12-15 21:17:31 +0100

    update GTM

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,14 +29,14 @@ export default class CustomDocument extends Document {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-58RH9WF');`
+            })(window,document,'script','dataLayer','GTM-P3PG3KG');`
           }}
         />
 
         <body className="loading">
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58RH9WF"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P3PG3KG"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>`
             }}
           ></noscript>
