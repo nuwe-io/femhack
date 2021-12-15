bc1b613af531eb878cb4d9d88f88e74eda893e7f
Edgar <edgargc.upc@gmail.com>
2021-12-15 21:11:44 +0100

    update GTM

--- components/sponsor-section.tsx
+++ components/sponsor-section.tsx
@@ -65,7 +65,7 @@ export default function SponsorSection({ sponsor }: Props) {
             >
               {sponsor.callToAction}
             </a>
-            <a
+            {/* <a
               href={sponsor.discord}
               target="_blank"
               rel="noopener noreferrer"
@@ -73,7 +73,7 @@ export default function SponsorSection({ sponsor }: Props) {
               className={cn(styles.button, styles['button-link'])}
             >
               Chat on Discord
-            </a>
+            </a> */}
           </div>
           <div className={styles.resources}>
             <h2 className={styles.heading}>Resources</h2>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,43 +21,25 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <meta name="facebook-domain-verification" content="3kzycq9gwzyuuklcwpazlynzqrf9qk" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6PBT74YNZ2"></script>
+        {/* GOOGLE TAG MANAGER - SCRIPT */}
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-6PBT74YNZ2');`
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-58RH9WF');`
           }}
         />
-
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '593824471730067'); 
-            fbq('track', 'PageView');`
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=593824471730067&ev=PageView&noscript=1"
-          />
-        </noscript>
 
         <body className="loading">
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58RH9WF"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
