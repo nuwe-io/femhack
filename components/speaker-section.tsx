25448ded63ed3f6dc8c9d3b00e5b1712334192e9
Edgar <edgargc.upc@gmail.com>
2022-01-17 12:54:32 +0100

    no speaker social media

--- components/speaker-section.tsx
+++ components/speaker-section.tsx
@@ -60,36 +60,6 @@ export default function SpeakerSection({ speaker }: Props) {
             </p>
             <h2 className={styles['bio-header']}>Bio</h2>
             <p className={styles.bio}>{speaker.bio}</p>
-            <h3 className={styles['socials-header']}>Social Media</h3>
-            {speaker.twitter ? (
-              <a
-                aria-label="Twitter"
-                href={speaker.twitter}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <TwitterIcon />
-              </a>
-            ) : (
-              <span className={styles.disabled}>
-                <TwitterIcon />
-              </span>
-            )}
-            {speaker.github ? (
-              <a
-                aria-label="GitHub"
-                className={styles.githubIcon}
-                href={speaker.github}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GithubIcon color="#D8D8D8" size={24} />
-              </a>
-            ) : (
-              <span className={cn(styles.githubIcon, styles.disabled)}>
-                <GithubIcon color="#D8D8D8" size={24} />
-              </span>
-            )}
           </div>
         </div>
       </div>
