d93a91c73e37ba2e8ce44c37a2c89347ec4a284d
Edgar <edgargc.upc@gmail.com>
2022-01-19 13:36:47 +0100

    add calendar button

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
 import styles from './header.module.css';
+import buttonStyle from './buttons.module.css';
 
 type Props = {
   hero: React.ReactNode;
@@ -10,6 +11,23 @@ export default function Header({ hero, description }: Props) {
     <>
       <h1 className={styles.hero}>{hero}</h1>
       <p className={styles.description}>{description}</p>
+      <div style={{ display: 'flex', margin: 'auto' }}>
+        <div style={{ margin: '30px 0px' }}>
+          {hero === 'Schedule' ? (
+            <button
+              className={buttonStyle.button}
+              onClick={() =>
+                window.open(
+                  'https://calendar.google.com/calendar/u/0?cid=Y19ycWw5c2docms2MjFxNTNscWMxYzNmNHBpY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t',
+                  '_blank'
+                )
+              }
+            >
+              Add schedule to my calendar
+            </button>
+          ) : null}
+        </div>
+      </div>
     </>
   );
 }
